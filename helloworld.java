ae8c72a9a2818cbf88ea0218c1829ae4efa1227a
venkat <pathapativenkat319@gmail.com>
2023-12-23 09:18:45 +0530

    added new info

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Start {
     public static void main(String[] args) {
-        System.out.println("congratulations");
+        System.out.println("congratulations you succesfully completed your first program");
     }
 }
